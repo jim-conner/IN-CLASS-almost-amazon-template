abeae7710d9db4f7692ac7785db2daf8de568b2a
Jim Administrator <jamesdavidconner@gamil.com>
2021-02-27 17:37:53 -0600

    add author function works but delete does not

--- src/javascripts/components/authors.js
+++ src/javascripts/components/authors.js
@@ -1,10 +1,23 @@
 const showAuthors = (array) => {
-  document.querySelector('#store').innerHTML = '';
-  // CREATE A BUTTON TO ADD BOOKS
+// CREATE A BUTTON TO ADD BOOKS
+  document.querySelector('#add-button').innerHTML = '<button class="btn btn-success btn-lg mb-4" id="add-author-btn">Add An Author</button>';
 
+  document.querySelector('#store').innerHTML = '';
+  document.querySelector('#form-container').innerHTML = '';
+
+  // STUDENTS create cards for your authors
   array.forEach((item) => {
-    // FIXME: STUDENTS create cards for your authors
-    document.querySelector('#store').innerHTML += `${item}`;
+    document.querySelector('#store').innerHTML += `<div class="card">
+    <div class="card-body" style="height: 180px;">
+    <h5 class="card-title">${item.first_name} ${item.last_name}</h5>
+    <p class="card-text">${item.email}</p>
+    <hr>
+    <button class="btn btn-info" data-toggle="modal" data-target="#formModal" id="edit-author-btn--${item.firebaseKey}">Edit Author</button>
+    <button class="btn btn-danger" id="delete-author--${item.firebaseKey}">Delete Author</button>
+    </div>
+  </div>`;
+
+    document.querySelector('#store').innerHTML += `${item.first_name}`;
   });
 };
 
--- src/javascripts/events/domEvents.js
+++ src/javascripts/events/domEvents.js
@@ -1,23 +1,35 @@
 import addBookForm from '../components/forms/addBookForm';
+import addAuthorForm from '../components/forms/addAuthorForm';
+import { showBooks } from '../components/books';
+import { createBook, deleteBook } from '../helpers/data/bookData';
+import { createAuthor } from '../helpers/data/authorData';
 
 const domEvents = () => {
   document.querySelector('body').addEventListener('click', (e) => {
     // CLICK EVENT FOR DELETING A BOOK
     if (e.target.id.includes('delete-book')) {
       if (window.confirm('Want to delete?')) {
-        console.warn('CLICKED DELETE BOOK', e.target.id);
+        // pull the firebaseKey off the button
+        const firebaseKey = e.target.id.split('--')[1];
+        deleteBook(firebaseKey).then((booksArray) => showBooks(booksArray));
       }
     }
-
     // CLICK EVENT FOR SHOWING FORM FOR ADDING A BOOK
     if (e.target.id.includes('add-book-btn')) {
-      console.warn('CLICKED ADD BOOK BUTTON', e.target.id);
       addBookForm();
     }
 
     // CLICK EVENT FOR SUBMITTING FORM FOR ADDING A BOOK
     if (e.target.id.includes('submit-book')) {
-      console.warn('CLICKED SUBMIT BOOK', e.target.id);
+      e.preventDefault();
+      const bookObject = {
+        title: document.querySelector('#title').value,
+        image: document.querySelector('#image').value,
+        price: document.querySelector('#price').value,
+        sale: document.querySelector('#sale').value,
+        author_id: document.querySelector('#author').value,
+      };
+      createBook(bookObject).then((booksArray) => showBooks(booksArray));
     }
 
     // CLICK EVENT FOR SHOWING MODAL FORM FOR ADDING A BOOK
@@ -31,8 +43,23 @@ const domEvents = () => {
     }
 
     // ADD CLICK EVENT FOR DELETING AN AUTHOR
+
     // ADD CLICK EVENT FOR SHOWING FORM FOR ADDING AN AUTHOR
+    if (e.target.id.includes('add-author-btn')) {
+      addAuthorForm();
+    }
+
     // ADD CLICK EVENT FOR SUBMITTING FORM FOR ADDING AN AUTHOR
+    if (e.target.id.includes('submit-author')) {
+      e.preventDefault();
+      const authorObject = {
+        email: document.querySelector('#author-email').value,
+        firebaseKey: '',
+        first_name: document.querySelector('#first-name').value,
+        last_name: document.querySelector('#last-name').value,
+      };
+      createAuthor(authorObject).then((authorsArray) => showBooks(authorsArray));
+    }
     // ADD CLICK EVENT FOR EDITING AN AUTHOR
   });
 };
